a098c764c1bd15d7d746573a1cec4b932e42602f
juanmoralestorres82@gmail.com <juanmoralestorres82@gmail.com>
2022-05-17 20:38:25 -0300

    icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,19 +31,10 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
+           
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/juanmorales9/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +44,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/s.o.u.m.y.a_j.i.t/"
+                href="https://www.instagram.com/juanimorales9/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
