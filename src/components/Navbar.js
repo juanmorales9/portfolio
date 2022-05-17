0bf06483d4640af439c82ac87066334275094089
juanmoralestorres82@gmail.com <juanmoralestorres82@gmail.com>
2022-05-17 20:35:27 -0300

    jota

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -96,7 +96,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/juanmorales9/portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
